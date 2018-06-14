f6108e3b970122c8c32ecadb693c770a3c11dede
Rajesh Acharya <rajeshacharya.nma@gmail.com>
2018-06-14 23:07:00 +0530

    PUT POST DELETE methods

--- course-api/src/main/java/com/rajesh/springboot/course_api/Topic/TopicService.java
+++ course-api/src/main/java/com/rajesh/springboot/course_api/Topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.rajesh.springboot.course_api.Topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,16 +14,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("Java", "Core Java", "Core Java Description"),
-			new Topic("Javascript", "Javascript", "Javascript Description"));
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+					new Topic("Java", "Core Java", "Core Java Description"),
+					new Topic("Javascript", "Javascript", "Javascript Description")));
 
-	
 	public List<Topic> getAllTopics() {
 		return topics;
 	}
-	
+
 	public Topic getTopic(String id) {
 		return (Topic) topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void update(Topic topic, String id) {
+		for (int i = 0; i < topics.size(); i++) {
+			if (topics.get(i).getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
